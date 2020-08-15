a059062f2c92891a2addf009bb2c1ad79630e9c7
morokiane <morokiane@excite.com>
2020-08-15 09:21:48 -0600

    sun key

--- scripts/SunLock.gd
+++ scripts/SunLock.gd
@@ -5,17 +5,25 @@ onready var centerAnim = $center
 
 var canAddKey = false
 
+# signal openChurch
+
+""" Key names will be SpokeOne, SpokeTwo, SpokeThree, SpokeFour, SpokeFive, and CenterKey
+Spoke keys have to be named in the level in order to avoid having to make 5 separate prefabs """
+
 func _on_Area2D_body_entered(body):
     if body.is_in_group("player"):
         canAddKey = true
+        print("can add a key")
         
 func _on_Area2D_body_exited(body):
     if body.is_in_group("player"):
         canAddKey = false
+        print("can't add a key")
         
 func _process(_delta):
-    if Input.is_action_just_pressed("interact"):
+    if Input.is_action_just_pressed("interact") && canAddKey:
         AddKey()
 
 func AddKey():
+    print ("adding a key")
     pass
